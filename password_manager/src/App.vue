7a67ec5f84836f340fb773bda8404e32f8881be4
subhadip1203 <subhadip1203@gmail.com>
2022-01-02 23:57:41 +0530

    password_manager by vue

---
+++ password_manager/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <PasswordInput @changepassword="changePassword"/>
+</template>
+
+<script>
+import PasswordInput from './components/PasswordInput.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PasswordInput
+  },
+  data: function() {
+    return {
+      mypassword: null
+    }
+  },
+  watch: {
+    mypassword: function(newval){
+      console.log(newval);
+    }
+  },
+  method: {
+    changePassword: function(payload){
+      if(payload){
+        this.mypassword = payload
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ password_manager/src/components/PasswordInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" v-model="mypass"/>
+  <button @click="savePasswordHandler"> submit password</button>
+</template>
+
+<script>
+export default {
+  name: 'PasswordInput',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      mypass: ''
+    }
+  },
+  methods: {
+    savePasswordHandler : function(){
+      this.$$emit("changepassword" , this.mypass)
+    }
+  }
+}
+</script>
