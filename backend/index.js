4f2177e516deb863733ae170c9f13ea5eff7f906
subhadip1203 <subhadip1203@gmail.com>
2022-01-09 23:43:50 +0530

    post req

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.post('/', (req, res) => {
+  res.send('post data!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
