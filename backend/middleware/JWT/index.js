cd7ebad72003f2018a101fb0136a714c28b6c1eb
subhadip1203 <subhadip1203@gmail.com>
2022-05-22 23:48:06 +0530

    auth middleware 2

--- backend/middleware/JWT/index.js
+++ backend/middleware/JWT/index.js
@@ -1,25 +1,26 @@
 const { verifyAccessToken} = require("../../utils/jwt");
 const {catchAsyncMiddleware} = require("../../utils/error")
+const { PublicError, PrivateError } = require("../../utils/error");
 
 const authMiddleWare = catchAsyncMiddleware(async (req, res, next) => {
   if(req.headers.authorization ) {
     const [data,error] = await verifyAccessToken(req.headers.authorization)
-    //--- if all ok -----//
+  
     if(data !== null && error === null ){
       res.locals.user = {...data}
       next()
     }
-    //--- if token valid but expired -----//
+    
     else if(data === null && error ==='TokenExpiredError') {
-      res.status(400).send({message: "token expired"})
+      throw new PublicError("token expired");
     }
-    //--- if token invalid -----//
+    
     else{
-      res.status(400).send({message: "auth error"})
+      throw new PublicError("Auth error");
     }
   }
   else{
-    res.status(400).send({message: "missing tokens error"})
+    throw new PublicError("missing tokens error");
   }
 })
 
